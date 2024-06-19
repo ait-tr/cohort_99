ea5259ed0bd198fb0e0039f0b0850f63fee4d8c8
Bill Johnson <example1@gmail.com>
2024-06-19 14:37:07 +0000

    (HW)-user: 'Bill_Johnson-example1@gmail.com', commit time: '2024-06-19 16:37:07'

---
+++ basic_programming/lesson_02/hw_solution/HW_02.java
@@ -0,0 +1,11 @@
+public class HW_02 {
+    public static void main(String[] args) {
+        String myName = "Bill Johnson";
+        String myAge = "35";
+        String myCity = "Berlin";
+
+        System.out.println("My name is" + " " + myName + ".");
+        System.out.println("I am " + myAge + " years old.");
+        System.out.println("Currently I live in " + myCity + ".");
+    }
+}
