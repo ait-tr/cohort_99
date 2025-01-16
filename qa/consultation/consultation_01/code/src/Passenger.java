1ce3759d0274fdac6e39f4712cba9b953904d873
AIT-Reutow <andrey.reutow@ait-tr.de>
2025-01-16 16:18:58 +0000

    update

---
+++ qa/consultation/consultation_01/code/src/Passenger.java
@@ -0,0 +1,18 @@
+/**
+ * 1/24/2024
+ * consultation_sidikov_01
+ *
+ * @author Marsel Sidikov (AIT TR)
+ */
+public class Passenger {
+
+    private final String name;
+
+    public Passenger(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
